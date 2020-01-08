53b41d9ce4ba434c0c914e4d9f50d533ffd584a3
Trisha Parikh <trisha.cba1722@ict.gnu.ac.in>
2020-01-08 05:30:41 +0000

    ttttttt

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Weather decided by Trisha</p>
+        <p className="p-3 text-light h3 bg-primary">Weather decided by Trishap>
     </div>
 }
   
